cf16eee2037816e3010b1ca256df4cf198ee8286
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2013-11-20 17:23:26 +0100

    project now compile

--- lib/supervisorring.ex
+++ lib/supervisorring.ex
@@ -1,7 +1,7 @@
 defmodule Supervisorring do
   def global_sup_ref(sup_ref), do: "#{sup_ref}_global_sup"
   def child_manager_ref(sup_ref), do: "#{sup_ref}_child_manager"
-  def local_sup_ref(sup_ref), do: supref
+  def local_sup_ref(sup_ref), do: sup_ref
 
   defmodule GlobalSup do
     use Supervisor.Behaviour
@@ -36,32 +36,34 @@ defmodule Supervisorring do
       def start_link(sup_ref,specs,callback), do:
         :supervisor.start_link({:local,sup_ref|>Supervisorring.child_manager_ref},__MODULE__,{sup_ref,specs,callback})
       def init({sup_ref,child_specs,callback}), do:
-        handle_cast(:sync_children,State[sup_ref: sup_ref,child_specs: specs,callback: callback])
+        handle_cast(:sync_children,State[sup_ref: sup_ref,child_specs: child_specs,callback: callback])
       def handle_cast(:sync_children,State[sup_ref: sup_ref,child_specs: specs,callback: callback]=state) do
         ring = :gen_event.call(Supervisorring.Server.RingListener,:get_ring)
         cur_children = :supervisor.which_children(sup_ref|>Supervisorring.local_sup_ref) |> reduce(HashDict.new,fn {id,_,_,_}=e,dic->dic|>Dict.put(id,e) end)
+        ## the tricky point is here, take only child specs with an id which is associate with the current node in the ring
         wanted_children = expand_specs(specs)|>filter(fn {id,_,_,_}->(ring|>ConsistentHash.node_for_key(id)) == node() end)
-                                           |>reduce(HashDict.new,fn {id,_,_,_,_,_}=e,dic->dic|>Dict.put(id,e) end)
+                                             |>reduce(HashDict.new,fn {id,_,_,_,_,_}=e,dic->dic|>Dict.put(id,e) end)
+        ## kill all the local children which should not be in the node, get/start child on the correct node to migrate state if needed
         cur_children |> filter(&not(Dict.has_key?(wanted_children,&1))) |> each fn {id,child,type,modules}->
-          case child do
-            :undefined ->
-            :restarted ->
-            oldpid ->
-              {:already_started,newpid} = :rpc.call(ring|>ConsistentHash.node_for_key(id),
-                :supervisor,:start_child,[sup_ref|>Supervisorring.local_sup_ref,wanted_children|>Dict.get(id)])
-              callback.migrate({id,type,modules},oldpid,newpid)
-              sup_ref |> Supervisorring.local_sup_ref |> :supervisor.terminate_child(id)
-              sup_ref |> Supervisorring.local_sup_ref |> :supervisor.delete_child(id)
+          new_node = ring|>ConsistentHash.node_for_key(id)
+          if is_pid(child) do
+            case :rpc.call(new_node,:supervisor,:start_child,[sup_ref|>Supervisorring.local_sup_ref,wanted_children|>Dict.get(id)]) do
+              {:error,{:already_started,existingpid}}->callback.migrate({id,type,modules},child,existingpid)
+              {:ok,newpid}->callback.migrate({id,type,modules},child,newpid)
+              _ -> :nothingtodo
+            end
+            sup_ref |> Supervisorring.local_sup_ref |> :supervisor.terminate_child(id)
           end
+          sup_ref |> Supervisorring.local_sup_ref |> :supervisor.delete_child(id)
         end
-        wanted_children |> filter(&not(Dict.has_key?(cur_children,&1)) |> each fn childspec-> 
+        wanted_children |> filter(&not(Dict.has_key?(cur_children,&1))) |> each fn childspec-> 
           :supervisor.start_child(sup_ref|>Supervisorring.local_sup_ref,childspec)
         end
         state
       end
       defp expand_specs(specs) do
-        {child_specs,spec_generators} = specs |> partition &is_tuple
-        concat(child_specs,spec_generators |> flat_map &(&1()))
+        {child_specs,spec_generators} = specs |> partition &is_tuple/1
+        concat(child_specs,spec_generators |> flat_map &(&1.()))
       end
     end
   end
@@ -70,13 +72,14 @@ end
 defmodule :supervisorring do
   use Behaviour
   import Supervisorring
+  alias Supervisorring.App.Sup, as: App
   @doc "process migration function, called before deleting a pid when the ring change"
-  defcallback migrate({id,type,modules},old_pid,new_pid)
+  defcallback migrate({id::atom(),type:: :worker|:supervisor, modules::[module()]|:dynamic},old_pid::pid(),new_pid::pid())
   @doc """
   callback called when a child has been started dynamically, should be used to
   maintain the global lists of processes returned by every {:child_spec_gen,fun}
   """
-  defcallback save_child(child_spec)
+  defcallback save_child(child_spec::term())
   @doc """
   supervisor standard callback, but with a new type of childspec to handle an
   external (global) source of child list (necessary for dynamic child starts,
@@ -84,7 +87,10 @@ defmodule :supervisorring do
   standard child_spec : {id,startFunc,restart,shutdown,type,modules}
   new child_spec : {:child_spec_gen,fun()->[:standard_child_spec]}
   """
-  defcallback init(args)
+  defcallback init(args::term())
+
+  def node_of(id), do:
+    :gen_event.call(Supervisorring.Events, App.SuperSup.NodesListener,:get_ring) |>ConsistentHash.node_for_key(id)
 
   @doc """
   start supervisorring process, which is a simple erlang supervisor, but the
@@ -93,28 +99,25 @@ defmodule :supervisorring do
   change if necessary to maintain the proper process distribution.
   """
   def start_link(name,module,args), do:
-    :supervisor.start_link(name,Supervisorring.Sup,{name,module,args})
+    :supervisor.start_link(name,GlobalSup,{name,module,args})
 
-  def start_child(supref,childspec) do
-    rpc.call(:gen_event.call({Supervisorring.ChildManager,sup_ref},:get_ring)|>ConsistentHash.node_for_key(id),
-        :supervisor,:start_child,[sup_ref|>local_sup_ref,childspec])
+  def start_child(supref,{id,_,_,_,_,_}=childspec) do
+    :rpc.call(node_of(id),:supervisor,:start_child,[supref|>local_sup_ref,childspec])
   end
 
   def terminate_child(supref,id) do
-    rpc.call(:gen_event.call({Supervisorring.ChildManager,sup_ref},:get_ring)|>ConsistentHash.node_for_key(id),
-       :supervisor,:terminate_child,[sup_ref|>local_sup_ref,id])
+    :rpc.call(node_of(id),:supervisor,:terminate_child,[supref|>local_sup_ref,id])
   end
 
   def restart_child(supref,id) do
-    rpc.call(:gen_event.call({Supervisorring.ChildManager,sup_ref},:get_ring)|>ConsistentHash.node_for_key(id),
-       :supervisor,:restart_child,[sup_ref|>local_sup_ref,id])
+    :rpc.call(node_of(id),:supervisor,:restart_child,[supref|>local_sup_ref,id])
   end
 
-  def which_children(supref) do
+  def which_children(_supref) do
     
   end
 
-  def count_children(supref) do
+  def count_children(_supref) do
     
   end
 end
--- lib/supervisorring_app.ex
+++ lib/supervisorring_app.ex
@@ -12,10 +12,7 @@ defmodule Supervisorring.App do
       ], strategy: :one_for_one)
     end
     defmodule SuperSup do
-      def init(nil) do
-        Process.flag(:trap_exit,true)
-        {:ok,nil}
-      end
+      import Enum
       defmodule NodesListener do
         def handle_event({:new_ring,oldring,newring},state) do
           case {oldring.up_set|>to_list,newring.up_set|>to_list} do
@@ -27,17 +24,20 @@ defmodule Supervisorring.App do
         def handle_call(:get_ring,ring), do: {:ok,ring,ring}
       end
       use GenServer.Behaviour
-      def init(args) do
+      def start_link, do: :gen_server.start_link({:local,__MODULE__},__MODULE__,[nil])
+      def init(nil) do
+        Process.flag(:trap_exit,true)
         :gen_event.add_sup_handler(NanoRing.Events,NodesListener,
           ConsistentHash.ring_for_nodes(:gen_server.call(NanoRing,:get_up)))
+        {:ok,nil}
       end
-      def handle_cast({:terminate,global_sup},state), do: exit(global_sup,:normal)
-      def handle_info({'EXIT',from,reason},state) when reason != :normal do
+      def handle_cast({:terminate,global_sup},nil), do: Process.exit(global_sup,:normal)
+      def handle_info({'EXIT',from,reason},nil) when reason != :normal do
         :gen_server.call(NanoRing,:get_up) |> Enum.each fn n ->
           :gen_server.cast({n,__MODULE__},{:terminate,from|>Process.info(:registered_name)})
         end
       end
-      def handle_info({:gen_event_EXIT,_,_},s), do: exit(:ring_listener_died)
+      def handle_info({:gen_event_EXIT,_,_},nil), do: exit(:ring_listener_died)
     end
   end
 end
