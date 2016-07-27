09606860797b8ff2fbe0728abb737d2d977d1ca9
Laurent Picouleau <laurent@kbrwadventure>
2016-07-27 09:53:42 +0200

    adding a DHT GenServer

---
+++ lib/DHT_gen_server.ex
@@ -0,0 +1,18 @@
+defmodule DHTGenServer do
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], [])
+  end
+
+  def init([]) do
+    {:ok, ConsistentHash.ring_for_nodes([node])}
+  end
+
+  def handle_call(:get_ring, ring), do: {:ok, ring, ring}
+
+  def handle_cast({:new_ring, new_up_set}, _) do
+    GenEvent.notify(Supervisorring.Events, :new_ring)
+    {:noreply, ConsistentHash.ring_for_nodes(new_up_set)}
+  end
+end
