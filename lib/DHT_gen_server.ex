414db6c644e3b65dcb1138b3f91ee7adb940ab16
Laurent Picouleau <laurent@kbrwadventure>
2016-08-05 15:15:52 +0200

    keeping several rings in DHTGenServer

--- lib/DHT_gen_server.ex
+++ lib/DHT_gen_server.ex
@@ -3,15 +3,22 @@ defmodule DHTGenServer do
 
   def start_link(_), do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
-  def init([]), do: {:ok, ConsistentHash.ring_for_nodes([node])}
+  def init([]) do
+    ring = ConsistentHash.ring_for_nodes([node()])
+    {:ok, Map.put(Map.new(), :default, ring)}
+  end
 
-  def get_ring(), do: GenServer.call(__MODULE__, :get_ring)
+  def get_ring(ring_name \\ :default),
+    do: GenServer.call(__MODULE__, {:get_ring, ring_name})
 
-  def handle_call(:get_ring, _, ring), do: {:reply, ring, ring}
+  def handle_call({:get_ring, ring_name}, _, rings),
+    do: {:reply, Map.get(rings, ring_name), rings}
 
-  def handle_cast({:new_ring, reason, new_up_set}, _) do
+  def handle_cast({:new_ring, reason, new_up_set}, rings),
+    do: handle_cast({:new_ring, reason, new_up_set, :default}, rings)
+  def handle_cast({:new_ring, reason, new_up_set, ring_name}, rings) do
     ring = ConsistentHash.ring_for_nodes(new_up_set)
     GenEvent.notify(Supervisorring.Events, {:new_ring, reason})
-    {:noreply, ring}
+    {:noreply, Map.put(rings, ring_name, ring)}
   end
 end
